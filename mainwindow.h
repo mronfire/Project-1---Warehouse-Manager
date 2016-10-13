d279ea1bbfa25518eac96501aca3e559986cf52f
codenameCodename <ehiwolfgang@gmail.com>
2016-10-12 18:44:13 -0700

    Got the stuff I needed from checkout

--- mainwindow.h
+++ mainwindow.h
@@ -2,7 +2,11 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
-#include "userwindow.h"
+#include "purchase.h"
+#include "sales.h"
+#include "member.h"
+#include "fileparsing.h"
+//#include "userwindow.h"
 
 namespace Ui {
 class MainWindow;
@@ -15,14 +19,20 @@ class MainWindow : public QMainWindow
 public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
+    member *memberList;
+    SalesDay *dayList;
 
 private slots:
-    void on_loginButton_clicked();
-
-    void on_pushButton_clicked();
+    void on_loginButton_clicked();                           //login buttom
+    void on_closeButton_clicked();                           //closes program
+    void on_addButton_clicked();                             //add member
+    void on_removeButton_clicked();                          //remove member
+    void on_viewButton_clicked();                            //view members
 
 private:
     Ui::MainWindow *ui;
+    QString memFile = "C:/Users/marod/Desktop/Project-1---T.E.A.M/members.txt";
+    QString purchFile = "C:/Users/marod/Desktop/Project-1---T.E.A.M/purchases.txt";
 };
 
 #endif // MAINWINDOW_H
